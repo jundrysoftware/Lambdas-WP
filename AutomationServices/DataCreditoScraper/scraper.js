56614dd0dd4cb081ab589aa2c7ec808365b317b7
Andres <andresmorelos123@gmail.com>
2021-02-03 20:38:35 -0500

    Updating scrapping to work with new datacredito login page.

--- AutomationServices/DataCreditoScraper/scraper.js
+++ AutomationServices/DataCreditoScraper/scraper.js
@@ -9,38 +9,42 @@ const {
 } = process.env
 
 const start = async (event, context) => {
-    console.info('Getting User Config')
-    // This function open the mongo connection
-    const user = await getUser({ emails: EMAIL_USERNAME })
-   
-    const username = decrypt(user.settings.datacredito.user)
-    const password = decrypt(user.settings.datacredito.password)
-    const secondpass = decrypt(user.settings.datacredito.secondpass)
+    try {
+        console.info('Getting User Config')
+        // This function open the mongo connection
+        const user = await getUser({ emails: EMAIL_USERNAME })
 
-    console.info('Getting Session Id')
-    const sessionid = await getSession(username, password, secondpass)
-    
-    console.info('Getting History Data')
-    const history = await getHistory(sessionid)
+        const username = decrypt(user.settings.datacredito.user)
+        const password = decrypt(user.settings.datacredito.password)
+        const secondpass = decrypt(user.settings.datacredito.secondpass)
 
-    const date = new Date()
+        console.info('Getting Session Id')
+        const sessionid = await getSession(username, password, secondpass)
 
-    const historyObject = {
-        comportamiento: history.diagnostic.endeudamiento.comportamiento,
-        score: history.score.score,
-        amountOfProducts: history.diagnostic.estadoPortafolio.numeroProductosAbiertosAlDia,
-        arrears30daysLastYear: history.diagnostic.habitoPago.productosConMora30DiasUltimos12Meses,
-        arrears60daysLast2Year: history.diagnostic.habitoPago.productosConMora60DiasUltimos48Meses,
-        arrearsAmount: history.diagnostic.habitoPago.saldoEnMora,
-        date: {
-            month: date.getMonth() + 1,
-            year: date.getFullYear()
-        },
-        user: user._id
+        console.info('Getting History Data')
+        const history = await getHistory(sessionid)
+
+        const date = new Date()
+
+        const historyObject = {
+            comportamiento: history.diagnostic.endeudamiento.comportamiento,
+            score: history.score.score,
+            amountOfProducts: history.diagnostic.estadoPortafolio.numeroProductosAbiertosAlDia,
+            arrears30daysLastYear: history.diagnostic.habitoPago.productosConMora30DiasUltimos12Meses,
+            arrears60daysLast2Year: history.diagnostic.habitoPago.productosConMora60DiasUltimos48Meses,
+            arrearsAmount: history.diagnostic.habitoPago.saldoEnMora,
+            date: {
+                month: date.getMonth() + 1,
+                year: date.getFullYear()
+            },
+            user: user._id
+        }
+
+        console.info('Saving entry')
+        await updateOrCreate(historyObject)
+    } catch (error) {
+        console.error(error)
     }
-
-    console.info('Saving entry')
-    await updateOrCreate(historyObject)
 
 }
 
